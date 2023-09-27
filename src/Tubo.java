2c531fd95797a4d8cd87e5cfd9aec98ed995980c
Samuel <samuel.tardiota@buonarroti.tn.it>
2023-09-27 16:46:15 +0200

    feat: Exception nel costruttore Tubo

--- src/Tubo.java
+++ src/Tubo.java
@@ -3,8 +3,11 @@ public class Tubo {
     private float raggio;
     private float altezza;
 
-    public Tubo(float r, float a){
-        raggio = r;
+    public Tubo(float r, float a) throws Exception{
+        if(r<0 || a<0) {
+            throw new Exception("Dati non validi");
+        }
+        this.raggio = r;
         this.altezza = a;
     }
 
